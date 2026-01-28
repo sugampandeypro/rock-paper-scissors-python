2c2b6fd0c6e86ecc63017e839e83842ed375ba71
Sugam <ktw72@txstate.edu>
2026-01-28 14:06:14 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,80 +1,54 @@
 import random
-
-#Asking player for their choice
-player = int(input
-             ("What do you choose? Type 0 for ""rock"
-                   " 1 for paper or 2 for scissors\n"))
-
-if player==0:
-    print("You choose a rock")
-    print('''
+rock = '''
     _______
 ---'   ____)
       (_____)
       (_____)
       (____)
 ---.__(___)
-''')
-elif player==1:
-    print("You choose a paper")
-    print('''
+'''
+
+paper = '''
     _______
 ---'   ____)____
           ______)
           _______)
          _______)
 ---.__________)
-''')
-elif player==2:
-    print("You choose a scissors")
-    print('''
+'''
+
+scissors = '''
     _______
 ---'   ____)____
           ______)
        __________)
       (____)
 ---.__(___)
-''')
-else:
-    print("You made a invalid chooice.Please choose 0,1 or 2")
+'''
 
+game_images=[rock,paper,scissors]
+
+#Asking player for their choice
+player_choice = int(input
+             ("What do you choose? Type 0 for ""rock"
+                   " 1 for paper or 2 for scissors?...\n"))
+
+if player_choice>=0 and player_choice<=3:
+    print(f"Player choose:")
+    print(game_images[player_choice])
+    
 #computer randomly chooses
-computer=random.randint(0,2)
-if computer==0:
-    print("Computer choose a rock")
-    print('''
-        _______
-    ---'   ____)
-          (_____)
-          (_____)
-          (____)
-    ---.__(___)
-    ''')
-elif computer==1:
-        print("Computer choose a paper")
-        print('''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-''')
-elif computer==2:
-    print("Computer chooses a scissors")
-    print('''
-        _______
-    ---'   ____)____
-              ______)
-           __________)
-          (____)
-    ---.__(___)
-    ''')
+computer_choice=random.randint(0,2)
+if computer_choice>=0 and computer_choice<=3:
+    print("Computer choose:")
+    print(game_images[computer_choice])
+    
 
-if player==computer:
+
+if player_choice==computer_choice:
     print("It is a draw")
 
-elif ( player==0 and computer==2) or (player==1 and computer==0) or (player==2 and computer==1):
+elif ( player_choice==0 and computer_choice==2) or (player_choice==1 and computer_choice==0) or (player_choice==2 and computer_choice==1):
     print("Player wins")
 
 else:
